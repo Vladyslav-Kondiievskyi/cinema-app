8f6e13638361a489a0f09cc2134cf87a913e16b2
Vladyslav Kondiievskyi <80918559+Vladyslav-Kondiievskyi@users.noreply.github.com>
2022-11-14 22:42:58 +0200

    <commit message>

---
+++ src/main/java/cinema/config/DataInitializer.java
@@ -0,0 +1,41 @@
+package cinema.config;
+
+import cinema.model.Role;
+import cinema.model.User;
+import cinema.service.RoleService;
+import cinema.service.UserService;
+import java.util.Set;
+import javax.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer {
+    private final RoleService roleService;
+    private final UserService userService;
+
+    public DataInitializer(RoleService roleService,
+                           UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    public void inject() {
+        Role adminRole = new Role();
+        adminRole.setRoleName(Role.RoleName.ADMIN);
+        roleService.add(adminRole);
+        Role userRole = new Role();
+        userRole.setRoleName(Role.RoleName.USER);
+        roleService.add(userRole);
+        User admin = new User();
+        admin.setEmail("admin2022@i.ua");
+        admin.setPassword("admin2022");
+        admin.setRoles(Set.of(adminRole));
+        User user = new User();
+        user.setEmail("user2022@gmail.com");
+        user.setPassword("user2022");
+        user.setRoles(Set.of(userRole));
+        userService.add(admin);
+        userService.add(user);
+    }
+}
---
+++ src/main/java/cinema/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package cinema.config;
+
+import cinema.service.impl.CustomUserDetailsService;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomUserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(CustomUserDetailsService userDetailsService,
+                          PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/register").permitAll()
+                .antMatchers(HttpMethod.GET,"/cinema-halls").hasAnyRole("ADMIN","USER")
+                .antMatchers(HttpMethod.POST,"/cinema-halls").hasAnyRole("ADMIN","USER")
+                .antMatchers(HttpMethod.GET,"/movies").hasAnyRole("ADMIN","USER")
+                .antMatchers(HttpMethod.POST,"/movies").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/movie-sessions/available")
+                .hasAnyRole("ADMIN","USER")
+                .antMatchers(HttpMethod.POST,"/movie-sessions").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,"/movie-sessions/{id}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE,"/movie-sessions/{id}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/order").hasRole("USER")
+                .antMatchers(HttpMethod.POST,"/order/complete").hasRole("USER")
+                .antMatchers(HttpMethod.PUT,"/shopping-carts/movie-sessions").hasRole("USER")
+                .antMatchers(HttpMethod.GET,"/shopping-carts/by-user").hasRole("USER")
+                .antMatchers(HttpMethod.GET,"/users/by-email").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+}
